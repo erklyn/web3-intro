c68297f914e936bc3ae3489bfba0b8e06fca8c56
erklyn <uenesgursel@gmail.com>
2021-12-28 16:32:09 +0300

    counter with sol

---
+++ contracts/counter.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Counter {
+    uint counter;
+
+    function count() public {
+        counter++;
+        console.log("Counter is now", counter);
+    }
+    function getCounter() public view returns (uint32){
+        return uint32(counter);
+    }
+}
